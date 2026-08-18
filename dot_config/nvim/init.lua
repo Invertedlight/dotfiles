ed93897abdb59efdee6bb3e8b9b2e055bd0bca5c
Invertedlight <james.richardson@invertedlight.com>
2026-08-18 16:21:38 +0000

    basename key map add

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.keymap.set("n", "<leader>b", function()
+  local line = vim.fn.getline(".")
+  vim.fn.setreg("0", vim.fn.fnamemodify(line, ":t"))
+end, { noremap = true, silent = true })
